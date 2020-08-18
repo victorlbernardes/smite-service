5f0f354c71d6fe761d4e71c220362eaddc90eaae
victorlbernardes <victorlbernardes@gmail.com>
2020-08-18 10:20:10 -0300

    Update data.sql
    Alterando para ter apenas um Login

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,11 +8,5 @@ CREATE TABLE player (
   last_session_id VARCHAR(250) DEFAULT NULL
 );
 
-INSERT INTO player (player_id, username, password) VALUES
-  ('12345', 'Admin', '$2y$12$6dXGRIV0.F15uP6nx8.oru9oZ7oNR4l5CaHahlkLNb2RDib.1ZU5G'),
-  ('000', 'Akillian', '$2y$12$6dXGRIV0.F15uP6nx8.oru9oZ7oNR4l5CaHahlkLNb2RDib.1ZU5G'),
-  ('111', 'Celtzero', '$2y$12$6dXGRIV0.F15uP6nx8.oru9oZ7oNR4l5CaHahlkLNb2RDib.1ZU5G'),
-  ('222', 'Daviola2', '$2y$12$6dXGRIV0.F15uP6nx8.oru9oZ7oNR4l5CaHahlkLNb2RDib.1ZU5G'),
-  ('333', 'DosAnjos', '$2y$12$6dXGRIV0.F15uP6nx8.oru9oZ7oNR4l5CaHahlkLNb2RDib.1ZU5G'),
-  ('444', 'Mantovani', '$2y$12$6dXGRIV0.F15uP6nx8.oru9oZ7oNR4l5CaHahlkLNb2RDib.1ZU5G'),
-  ('555', 'ZTrivium', '$2y$12$6dXGRIV0.F15uP6nx8.oru9oZ7oNR4l5CaHahlkLNb2RDib.1ZU5G');
+INSERT INTO player (username, password) VALUES
+  ('Team Maya', '$2y$12$6dXGRIV0.F15uP6nx8.oru9oZ7oNR4l5CaHahlkLNb2RDib.1ZU5G';
